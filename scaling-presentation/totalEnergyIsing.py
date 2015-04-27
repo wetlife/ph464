a340667c4a9ec9a01f95369a7cf54657d70b9382
Kyle Thomas <wetlife@gmail.com>
2015-04-27 14:52:19 -0700

    ising.py working and nearly ready for scaling presentation tomorrow

--- scaling-presentation/totalEnergyIsing.py
+++ scaling-presentation/totalEnergyIsing.py
@@ -1,20 +1,21 @@
 from __future__ import division
 import sys
+from numpy import ones,zeros,exp,arange,floor
+from numpy.random import random
 import matplotlib.pyplot as plt
-import numpy as np
 
-latticeSize = int(np.sqrt(float(sys.argv[1]))) # number of sites in a row or column of the lattice
-H = np.zeros((latticeSize,latticeSize)) # magnetic field
-J = 1 # strength of neighbor-interactions
-microstate = np.ones((latticeSize,latticeSize), int) # initial lattice state
-T = 1111.25 # temperature, units depend on kb
-kb = 1 # Boltzmann's constant
-beta = 1/(kb*T) # 1/(kb T)
-metropolizations = 100*latticeSize**2  # number of times to metropolize
+latticeSize         = int(sys.argv[1]) # number of sites in a row or column of the lattice
+H                   = zeros((latticeSize,latticeSize)) # magnetic field
+J                   = 1 # strength of neighbor-interactions
+microstate          = ones((latticeSize,latticeSize), int) # initial lattice state
+T                   = 1111.25 # temperature, units depend on kb
+kb                  = 1 # Boltzmann's constant
+beta                = 1/(kb*T) # 1/(kb T)
+metropolizations    = 1000  # number of times to metropolize
 
 def indexPicker(size):
     'randomly pick a single index'
-    return np.floor((size)*np.random.random())
+    return floor((size)*random())
 
 def val(i,j,array=microstate):
     'implements cyclic boundary conditions on lattice'
@@ -23,11 +24,7 @@ def val(i,j,array=microstate):
 def siteEnergy(i,j,siteValue=False):
     'calculate energy of site (i,j) when site has value siteValue'
     if not siteValue: siteValue = val(i,j)
-    return (val(i,j,H) - J*(val(i+1,j) + val(i,j+1)))*siteValue
-
-def localEnergyDifference(i,j):
-    'calculate energy about site (i,j) when site has value siteValue'
-    return -2*(val(i,j,H) - J*(val(i-1,j) + val(i,j-1) + val(i+1,j) + val(i,j+1)  ))*val(i,j)
+    return (val(i,j,H) + J*(val(i+1,j) + val(i,j+1)))*siteValue
 
 def totalEnergy(): # calc. latt. NRG w/a flipped val.
     'return total energy of lattice'
@@ -43,26 +40,33 @@ def totalEnergyDifference(i,j):
     microstate[i,j] *= -1
     return totalEnergy() - flippedEnergy
 
+#def siteEnergyDifference(i,j):
+#    returnEnergyDifference = 0
+#    for i in [i-1,i]:
+#        for j in [j-1,j]:
+#            returnEnergyDifference += siteEnergy(i,j)-siteEnergy(i,j,-1*val(i,j))
+#    return returnEnergyDifference
+
 ### iterate metropolis algorithm
-def metropolizer( ):
+def metropolizer(metropolizations=metropolizations):
     'metropolize microstate number of times equivalent to metropolizations'
     for metropolisIterationIndex in range(metropolizations):
         metropolisIterationIndex += 1
         i = indexPicker(latticeSize)
         j = indexPicker(latticeSize)
 
-        deltaEnergy = localEnergyDifference(i,j)
+#        if metropolisIterationIndex%10==0:
+#            print 'Metropolization ', metropolisIterationIndex, ': (', i, ',', j, ')', val(i,j)
+#            print "Current energy of microstate is ", totalEnergy()
+        deltaEnergy = totalEnergyDifference(i,j)
         if deltaEnergy <= 0:
             microstate[i%latticeSize,j%latticeSize] = -1*val(i,j)
-        elif np.random.random()<np.exp(-deltaEnergy/(kb*T)):
+        elif random()<exp(-deltaEnergy/(kb*T)):
             microstate[i%latticeSize,j%latticeSize] = -1*val(i,j)
 
 
-print 'There are ', latticeSize**2, ' sites.'
-print 'The lattice will be metropolized ', metropolizations,' times.'
 metropolizer()
 print 'total energy is ', totalEnergy()
-
 #plt.imshow(microstate,interpolation='none')
 #plt.show()
 #
@@ -83,8 +87,15 @@ print 'total energy is ', totalEnergy()
 #    print 'stored value ', energyAndTemperatureTuples[k]
 #
 
+    
 
+        ### TODO ###   
+#xxxxxxxxxxxxxxxxxxxFinish metropolis algorithm
+#----FAIL----       Does site energy difference == total energy difference?
+#xxxxxxxxxxxxxxxxxxxTest metropolis algorithm
 #----TODO----       Plot metropolized microstate total energy vs. T
+
+#TODO:
 #-implement some way to find totalEnergy(temperature), call this E(T) below
 #-function iterates over E(T) for several T-values
 #   -index with parametrized range of integers
