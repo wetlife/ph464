61dda3f2375811d365b4c23e5ac95d1ba93d9297
Kyle Thomas <wetlife@gmail.com>
2015-05-26 17:06:34 -0700

    little more done on acoustic-project/model; testing git push from wngr412-pc13

--- acoustic-project/model.py
+++ acoustic-project/model.py
@@ -1,6 +1,12 @@
 from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
+import pylab
 
 room = np.zeros((100,100))
-image = plt.imshow(room)
+room[0][0] = 1
+
+
+
+image = plt.imshow( room, interpolation='none')
+plt.show()
