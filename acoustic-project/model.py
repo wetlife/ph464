49c62a1b0e05b46ee70c3fafa1aa163247d54687
Kyle Thomas <wetlife@gmail.com>
2015-05-26 18:19:04 -0700

    define several quantities

--- acoustic-project/model.py
+++ acoustic-project/model.py
@@ -3,10 +3,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pylab
 
-room = np.zeros((100,100))
-room[0][0] = 1
+c = 300 # m/s
+room = np.zeros((size,size)) # room is represented by a matrix of positions
+size = 20 # m
+room[0][0] = 1 # an impulse applied to the system
+latestTime = 10 # max t-value in seconds
+dt = 0.1 # step in seconds
+positionSpacing = .01 
+
+for timeStep in range(0,latestTime,dt)
+    for m in range(size):
+	    for n in range(size):
+                # calculate the p-value for each node
+		# use 2d version of centered verlet's 
+		p[n][m] = 
 
 
 
-image = plt.imshow( room, interpolation='none')
-plt.show()
+    plt.imshow( room, interpolation='none')
+    plt.show()
+
