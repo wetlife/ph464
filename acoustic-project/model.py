1c3258068c988e4e64004aa7a3607a91b0245885
Kyle Thomas <wetlife@gmail.com>
2015-06-08 09:38:39 -0700

    - diffracting boundary conditions implemented - need to figure out spacing of gratings for diffraction effects - record time evolution of various grating spacings, which is   parameterized as notchWidth - print diffraction grating spacings and depth in plot title - random impulse implemented but commented - make random impulses fall off as a gaussian fncn. of time - implement a symmetric impulse of opposite press. at a symmetric point about the center of the room from the random impulse to balance press. throughought room

--- acoustic-project/model.py
+++ acoustic-project/model.py
@@ -2,23 +2,43 @@ from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+from random import random
 
-a = 300 # m/s
-size = 20 # m
-latestTime = 90 # max t-value in seconds
-dt = 0.1 # step in seconds
+plt.ion()
+plt.figure()
+
+c = 300 # m/s
+size = 200 # m
+latestTime = 90000 # max t-value in seconds
+dt = 0.005 # step in seconds
 dx = dy = .01 # dPosition = dx = dy; positions are in a squared-lattice
 pArray = np.zeros((3,size,size)) # p(T,N,M); matrices of room's pressure at times t-dt, t, and t+dt stored in a 3D array s.t. p[0]=pOldMatrix, p[1]=pMatrix, and p[2]=pNewMatrix
+percentageChanceOfImpulse = 10
+cyclesPerFrame = 3
+impulseFrequency = 3
+notchWidth = 70
+numNotches = size//notchWidth
+notchDepth = np.min((4*size//10,notchWidth))
 
 def p(timeStep,n,m):
     '''accesses matrices and enforces boundary conditions on them'''
     if n<=0 or m<=0 or n>=size or m>=size:
         return 0
+    # diffracting boundary conditions below
+    elif n < notchDepth and (m//numNotches)%2 == 0: 
+        return 0
     else:
         return pArray[timeStep][n][m]
 
-pArray[2][3][4] = 1 # supply an impulse at the initial instant
+X,Y = np.meshgrid(np.linspace(0, size*dx, size), np.linspace(0, size*dx, size))
 
+pArray[2][size/2][size/2] = 1 # supply an impulse at the initial instant
+pArray[1][size/2][size/2] = 1 # supply an impulse at the initial instant
+
+pArray[2] = np.exp(-((X-size*dx/2)**2 + (Y-size*dx/2)**2)/0.1**2)
+pArray[1] = np.exp(-((X-size*dx/2)**2 + (Y-size*dx/2)**2)/0.1**2)
+
+i = 0
 for timeStep in range(0,latestTime):
     # set the current- and old-matrix values to increment time
     pArray[0] = pArray[1]
@@ -27,9 +47,14 @@ for timeStep in range(0,latestTime):
 	    for n in range(size):
                 # calculate the p-value at each position 
 	        # use 2d version of centered verlet's
-                pArray[2][n][m] = 2*p(1,n,m) +p(0,n,m,) +(dt/dx)**2\
+                pArray[2][n][m] = 2*p(1,n,m) -p(0,n,m,) +(dt/dx)**2\
                         *( p(1,n+1,m) +p(1,n-1,m) +p(1,n,m+1) +p(1,n,m-1) \
                         -4*p(1,n,m) )
-    plt.imshow( pArray[2], interpolation='none')
-    plt.legend()
-    plt.show()
+#   if random() > 1-percentageChanceOfImpulse/100:
+#       pArray[2][int(random()*size)][int(random()*size)] = 1
+    i = i + 1
+    if i % cyclesPerFrame == 0:
+        plt.clf()
+        plt.imshow( pArray[2], interpolation='none')
+        plt.colorbar()
+        plt.draw()
